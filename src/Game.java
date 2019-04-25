5b7ca79b7f6de793a69abfa653c5175fc92665ad
mac0321 14 <mac0321.14@esqualidus.cec.ime.usp.br>
2019-04-25 12:01:14 -0300

    Atribuição dos eventos à Battle

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,69 @@
 public class Game{
 	private class Battle extends EventSet{
+		
 		private Player[] Ps ;
+		
+		private class attack extends Event {
+			attack(){
+				super(0);
+			}
+			public void action(){
+				
+			}
+			public String description() {
+				return "Yay";
+			}
+		}
+		private class changePokemon extends Event {
+			changePokemon(){
+				super(0);
+			}
+			public void action(){
+				
+			}
+			public String description() {
+				return "Yay";
+			}
+		}
+		private class flee extends Event {
+			flee(){
+				super(0);
+			}
+			public void action(){
+				
+			}
+			public String description() {
+				return "Yay";
+			}
+		}
+		private class useItem extends Event {
+			useItem(){
+				super(0);
+			}
+			public void action(){
+				
+			}
+			public String description() {
+				return "Yay";
+			}
+		}
+		
 		public Battle(Player[] Ps) {
 			this.Ps = Ps;
 		}
 		public  void run() {
-			
+			int[] E = new int[2];
+			while (true){ //rodadas
+				E[0] = Ps[0].decide();
+				E[1] = Ps[1].decide();
+				
+				int i = (E[1] > E[0])?E[1]:E[0];
+			}
 		}
 		
 	}
 	public static void main (String[] args) {
-		private Player[] Ps = new Pĺayer[2];
+		private Player[] Ps = [new Pĺayer(),new Player()];
 		Battle b = new Battle(Ps);
 		b.run();
 	}
--- src/Player.java
+++ src/Player.java
@@ -5,48 +5,8 @@ public class Player {
 	
 	Player(){
 	}
-	private class attack extends Event {
-		attack(){
-			super(0);
-		}
-		public void action(){
-			
-		}
-		public String description() {
-			return "Yay";
-		}
-	}
-	private class changePokemon extends Event {
-		changePokemon(){
-			super(0);
-		}
-		public void action(){
-			
-		}
-		public String description() {
-			return "Yay";
-		}
-	}
-	private class flee extends Event {
-		flee(){
-			super(0);
-		}
-		public void action(){
-			
-		}
-		public String description() {
-			return "Yay";
-		}
-	}
-	private class useItem extends Event {
-		useItem(){
-			super(0);
-		}
-		public void action(){
-			
-		}
-		public String description() {
-			return "Yay";
-		}
+	
+	public int decide() {
+		return (int) Math.floor(Math.random()*4);
 	}
 }
