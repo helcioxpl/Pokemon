911d00a63b07657e9dbccdd7e65bea993ca0fd5b
mac0321 04 <mac0321.04@ranulfo.cec.ime.usp.br>
2019-04-25 11:41:17 -0300

    Game class criada

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,18 @@
-public class Battle extends EventSet{
-	private Player[] Ps = new Pĺayer[2];
-	
-	Battle() {
+public class Game{
+	private class Battle extends EventSet{
+		private Player[] Ps ;
+		public Battle(Player[] Ps) {
+			this.Ps = Ps;
+		}
+		public  void run() {
+			
+		}
 		
 	}
+	public static void main (String[] args) {
+		private Player[] Ps = new Pĺayer[2];
+		Battle b = new Battle(Ps);
+		b.run();
+	}
 }
+
