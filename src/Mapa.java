2c1c78010cdc83f3fab3b25b43e6a541c6e30e20
mac0321 04 <mac0321.04@ranulfo.cec.ime.usp.br>
2019-05-02 11:01:40 -0300

    Mapa update 2

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,17 +1,59 @@
 import java.util.Random;
 
 public class Mapa extends Player {
-	int x;
-	int y;
-	int[][] mapa;// 0 representa gramado e 1 concreto
-	public Mapa(int x, int y, int[][] concretoGramado) {
+	private int x;
+	private int y;
+	static char[][] mapa;// G quando gramado, C quando concreto e P a posicao do player
+	public Mapa(int x, int y, char[][] concretoGramado) {
 		super("Pokemon selvagem");
 		mapa = concretoGramado;
 		this.x = x;
 		this.y = y;
+	}
+	public void imprimirMapa(int posicaoX, int posicaoY) {
+		int i = 0;
+		int j = 0;
+		while (i < x) {
+			while (j < y) {
+				System.out.print("- ");
+				j++;
+			}
+			System.out.println("");
+			j = 0;
+			while (j < y) {
+				if(posicaoX == i && posicaoY == j) {
+					System.out.print("|P");
+					j++;
+				}
+				else {
+					System.out.print("|"+mapa[i][j] );
+					j++;
+				}
+			}
+			System.out.println("|");
+			j = 0;
+			i++;
+		}
+		while (j < y) {
+			System.out.print("- ");
+			j++;
+		}
+		
+	}
+	public static void main (String[] args) {
 		Random gerador = new Random();
-		posicaoX = gerador.nextInt(x);
-		posicaoY = gerador.nextInt(y);
+		char[][] teste = new char[10][11];
+		int i,j;
+		for(i = 0; i < 10; i++) {
+			for(j = 0; j < 11; j++) {
+				teste[i][j] = 'C';
+			}
+		}
+		Mapa mapaTeste = new Mapa(10, 11, teste);
+		int posicaoXPlayer = gerador.nextInt(mapaTeste.x);
+		int posicaoYPlayer = gerador.nextInt(mapaTeste.y);
+		mapaTeste.imprimirMapa(posicaoXPlayer, posicaoYPlayer);
+		
 	}
 
 }
