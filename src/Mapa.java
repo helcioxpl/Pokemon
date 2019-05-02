2c9c04ac1c7d82df34e9324b55eba31e5e120e33
mac0321 04 <mac0321.04@ranulfo.cec.ime.usp.br>
2019-05-02 09:34:04 -0300

    Mapa update

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,7 +1,17 @@
+import java.util.Random;
 
-public class Mapa {
-	public Mapa(int x, int y) {
-		
+public class Mapa extends Player {
+	int x;
+	int y;
+	int[][] mapa;// 0 representa gramado e 1 concreto
+	public Mapa(int x, int y, int[][] concretoGramado) {
+		super("Pokemon selvagem");
+		mapa = concretoGramado;
+		this.x = x;
+		this.y = y;
+		Random gerador = new Random();
+		posicaoX = gerador.nextInt(x);
+		posicaoY = gerador.nextInt(y);
 	}
 
 }
