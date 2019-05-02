82b8f9bf320ff319a95db4dd8e10ae3d7a52dcea
Sergio Ariel Gonzales Fuentes <checho@Sergios-MacBook-Pro.local>
2019-05-01 21:42:48 -0300

    chgPokemon e nextPokemon

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,29 @@
 public class Player {
 	private String name;
 	Pokemon[] p = new Pokemon[6];
-	int atual = 0;
+	private int atual = 0;
 	
-	Player(){
+	public Player(){
 	}
 	
-	public void chgPokemon(int n){
-		this.atual = n;
+	public void chgPokemon(){
+		Pokemon aux;
+		int n = decide(6 - atual - 1) + atual + 1;
+		aux = p[n];
+		p[n] = p[atual];
+		p[atual] = aux;
 	}
-	public int decide() {
-		return (int) Math.floor(Math.random()*4);
+	public int decide(int x) {
+		return (int) Math.floor(Math.random()*(x));
+	}
+	public int getAtual() {
+		return atual;
+	}
+	public boolean nextPokemon() {
+		atual++;
+		if(atual == 6) {
+			return false;
+		}
+		return true;
 	}
 }
