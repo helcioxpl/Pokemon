f270474ae1373576ffc220ae31871c59466f8dfe
sergiofuentes <sergioagf10@gmail.com>
2019-05-15 23:38:50 -0300

    Erros pokenmon consertados

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class Pokemon {
 	String nome;
 	int Hp[] = new int[2];
