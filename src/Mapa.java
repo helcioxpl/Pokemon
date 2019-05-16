b17a5535b096c01d030f6a2aa829dc3f239cd83f
helcioxpl <helcioxpl@hotmail.com>
2019-05-15 22:55:04 -0300

    event Map

--- src/Mapa.java
+++ src/Mapa.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Mapa extends Player {
 	private int x;
 	private int y;
+
 	static char[][] mapa;// . quando gramado, ' ' quando concreto e P a posicao do player
 	private int[] pos = new int[2];
+
+	private Event[] Evs = new Event[5];
+	private int last = 0;
 
 	public Mapa(int x, int y, char[][] concretoGramado) {
 		this(x,y,concretoGramado,"Aleatório");
@@ -44,43 +48,48 @@ public class Mapa extends Player {
 
 	private boolean possibleMovement(int mov) {
 		if (mov < 2) return (pos[mov % 2] > 0);
-		else return (pos[mov % 2] < (mov % 2)?y:x);
-	}
-
-	private boolean achouPokemonSelvagem() {
-		return (2 == decide(3));
+		else return (pos[mov % 2] < ((mov % 2)?y:x));
 	}
 
 	class move extends Event {
 
 	}
 
-	public static void main (String[] args) {
-		Random gerador = new Random();
-		boolean batalhaJaOcorreu = false;
-		int mov;
+	class wildPokemonAppears extends Event {
+		public useItem(){
+			super("Wild Pokemon Appeared","You found a wild ");
+		}
+		public void action(){
+			this.atual = 5;
+			this.description += this.getAtual().getNome()+".Prepare for battle!";
+			this.add(new Battle(Ps));
+		}
+	}
 
-		int[] dim = { gerador.nextInt(7)+5 , gerador.nextInt(5)+15 };
+	private void add(Event e){
+		Evs[last] = e;
+		last++;
+	}
+
+	public static void main (String[] args) {
+		int[] dim = { r.nextInt(7)+5 , r.nextInt(5)+15 };
 		char[][] teste = new char[dim[0]][dim[1]];
 
 		for(int i = 0; i < dim[0]; i++)
 			for(int j = 0; j < dim[1]; j++) 
-				teste[i][j] = (gerador.nextBoolean())?' ':'.';
+				teste[i][j] = (r.nextBoolean())?' ':'.';
 
 		Mapa mapa = new Mapa(dim[0], dim[1], teste);
 		Player[] Ps = { mapa, new Player("A") };
-		mapa.pos[0] = gerador.nextInt(mapa.x);
-		mapa.pos[1] = gerador.nextInt(mapa.y);
+
+		mapa.pos[0] = r.nextInt(mapa.x);
+		mapa.pos[1] = r.nextInt(mapa.y);
 		mapa.imprimirMapa();
 
-		while(!batalhaJaOcorreu) {
-			if(mapa.AndandoNaGrama()) {
-				if(mapa.achouPokemonSelvagem()) {
-					System.out.println("Achou pokemon");
-					Battle batalha = new Battle(Ps);
-					//batalha.action();
-					return;
-				}
+		int mov;
+		while (1){
+			for(int i = 0; i < Evs.length; i++){
+				Evs[i].happen();
 			}
 			else {
 				mov = Ps[1].decide(4);
@@ -92,6 +101,8 @@ public class Mapa extends Player {
 				mapa.playerMoves(mov);
 				mapa.imprimirMapa();
 			}
+			if(mapa.AndandoNaGrama())
+				if(decide(3) == 2) add(new wildPokemonAppears);
 		}
 	}
 }
